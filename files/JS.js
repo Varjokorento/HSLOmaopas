2bf4e5c7e98880501cd20403d91f2dbf7d7aba1d
Kalle Pusa <kalle.pusa@gmail.com>
2018-06-22 16:05:01 +0300

    Lisätty bussitoiminnallisuus palvelimen kautta

--- files/JS.js
+++ files/JS.js
@@ -2,16 +2,112 @@ $( "#salimaster" ).click(function() {
     $( "#sali" ).toggle( "fast", function() {
     });
 });
-var luku = Math.floor(Math.random());
-var x = "Seuraava bussi metroasemalle:" + luku;
 
-function haeBussit() {
 
-    var x = "Seuraava bussi metroasemalle:" + luku;
+// function haeTiedot() {
+//     $.post({
+//         url: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+//         data: JSON.stringify({
+//             "query": "stop(id: 'HSL:1040129') { name lat lon wheelchairBoarding'}"}),
+//             contentType: 'application/json'
+//
+//         }).done(function (response) {
+//             console.log('');
+//         })
+//     };
 
-            $("#bussintiedot").append(x);
-            $("#metrobussintiedot").append(x);
-}
+//
+// haeTiedot()
 
-$( document ).ready( haeBussit() );
+var query =  "{'query': '{stops(name:'hertton') {id name wheelchairBoarding} }'}";
+var teksti = JSON.stringify(query);
 
+var keri = JSON.parse(teksti);
+console.log(keri);
+var string = "{stops(name: 'hertton') {id name wheelchairBoarding} }";
+
+$('button').click(function() {
+
+$.ajax({
+    method: "POST",
+    url: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+    data: keri})
+        .done(function (msg) {
+            alert("Data Saved: " + msg)})});
+
+
+
+// $('button').click(function() {
+//     event.preventDefault();
+//     var entry = $('#entry').val()
+//     console.log(entry);
+//
+//
+//
+
+var string = '{stop(id: "HSL:1173210") {name, lat, lon, wheelchairBoarding}}';
+console.log(string);
+$('button').click(function() {
+
+    fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
+
+        method: 'POST',
+        headers: {'Content-Type': 'application/graphql'},
+        body: string
+    })
+        .then(res => console.log(res.data));
+});
+
+// const fetch = createApolloFetch({
+//     uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+// });
+// function fets() {
+//
+//     fetch({
+//         query: `stop (id: "HSL:1040129") {
+//        name
+//        lat
+//        lon
+//        wheelchairBoarding
+//       }
+//       }`
+//     });
+// }
+//
+// fets();
+
+
+var url = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+var headers = {
+    Accept: 'application/json',
+    'User-Agent': 'request',
+    'Content-Type': 'application/json'
+};
+
+var options = {
+    method: 'post',
+    body: undefined,
+    json: true,
+    url: url,
+    headers: headers
+};
+
+var request = require("request");
+
+function makeRequest(options){
+    request(options, function (error, response, body) {
+        if (error) {
+            console.error('error posting json: ', error);
+            throw error;
+        }
+        var responseHeaders = response.headers;
+        var statusCode = response.statusCode;
+        console.log('Status code: ', statusCode);
+        console.log('Body: ', body);
+    });
+};
+
+options.body = {
+    query: "{stops(name: 'hertton') {id name wheelchairBoarding} }"
+};
+makeRequest(options);
--- palvelin.js
+++ palvelin.js
@@ -1,6 +1,152 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var cors = require('cors')
+var fetchnorm = require('isomorphic-fetch');
+const { createApolloFetch } = require('apollo-fetch');
+var request = require("request");
+
+//eka kutsu
+var headers = {
+    'Content-Type': 'application/graphql'
+};
+
+var dataString = '{stop(id: "HSL:1431143") {name, stoptimesWithoutPatterns {scheduledArrival, realtimeArrival}}}';
+
+var options = {
+    url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+var data = "ei toimi";
+var lahtoaikaarvio = 0;
+var lahtoaikaReaali = 0;
+var lahtoaikatod = 0;
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        data = body;
+        var parsittu = "";
+        for (var t = 0; t < 150; t++) {
+
+        parsittu += data[t];
+        }
+        lahtoaikaarvio = parsittu.substring(84, 89);
+        lahtoaikatod = secondsToTime(lahtoaikaarvio);
+        if (lahtoaikatod["m"] < 10) {
+        lahtoaikaparsittuna = lahtoaikatod["h"] + ":" + "0" + lahtoaikatod["m"];
+        } else {
+        lahtoaikaparsittuna = lahtoaikatod["h"] + ":" + lahtoaikatod["m"];
+        }
+        if (lahtoaikatod["h"] < 10) {
+        lahtoaikaparsittuna = "0" + lahtoaikatod["h"] + ":" + lahtoaikatod["m"]
+        }
+        if (lahtoaikatod["h"] < 10 && lahtoaikatod["m"] < 10) {
+        lahtoaikaparsittuna = "0" + lahtoaikatod["h"] + ":" + "0" + lahtoaikatod["m"]
+        }
+        
+        console.log(lahtoaikatod);
+        lahtoaikaReaali = parsittu.substring(108, 113);
+        
+    }
+}
+
+function secondsToTime(secs)
+{
+    var hours = Math.floor(secs / (60 * 60));
+
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+
+    var obj = {
+        "h": hours,
+        "m": minutes,
+    };
+    return obj;
+}
+
+
+
+request(options, callback);
+
+//tokakutsu 
+
+var headers = {
+    'Content-Type': 'application/graphql'
+};
+
+var dataString2 = '{stop(id: "HSL:1431139") {name, stoptimesWithoutPatterns {scheduledArrival, realtimeArrival}}}';
+
+var options2 = {
+    url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+    method: 'POST',
+    headers: headers,
+    body: dataString2
+};
+
+
+var lahtoaikaarvio2 = 0;
+var lahtoaikaReaali2 = 0;
+var lahtoaikatod2 = 0;
+
+function callback2(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        data = body;
+        var parsittu = "";
+        for (var t = 0; t < 150; t++) {
+
+        parsittu += data[t];
+        }
+        console.log(parsittu)
+        lahtoaikaarvio = parsittu.substring(90, 95);
+        console.log(lahtoaikaarvio)
+        lahtoaikatod = secondsToTime(lahtoaikaarvio);
+        if (lahtoaikatod["m"] < 10) {
+        lahtoaikaparsittuna2 = lahtoaikatod["h"] + ":" + "0" + lahtoaikatod["m"];
+        } else {
+        lahtoaikaparsittuna2 = lahtoaikatod["h"] + ":" + lahtoaikatod["m"];
+        }
+        if (lahtoaikatod["h"] < 10) {
+        lahtoaikaparsittuna2 = "0" + lahtoaikatod["h"] + ":" + lahtoaikatod["m"]
+        }
+        if (lahtoaikatod["h"] < 10 && lahtoaikatod["m"] < 10) {
+        lahtoaikaparsittuna2 = "0" + lahtoaikatod["h"] + ":" + "0" + lahtoaikatod["m"]
+        }
+        
+        console.log(lahtoaikatod);
+       
+        
+    }
+}
+
+function secondsToTime(secs)
+{
+    var hours = Math.floor(secs / (60 * 60));
+
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+
+    var obj = {
+        "h": hours,
+        "m": minutes,
+    };
+    return obj;
+}
+
+
+request(options2, callback2);
+
+app.use(cors())
 
 /*app.get('/', function (req, res) {
 	res.send('Hello node with Express');
@@ -10,9 +156,23 @@ var parser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('files'));
 
+app.set("view engine", "ejs"); 
+app.set("views", __dirname + "/files"); 
+
+app.get("/", (req, res) =>  { res.render("index", { arvioitulahtoaika: lahtoaikaparsittuna, arvioitusaapumisaika: lahtoaikaparsittuna2}); });
+
 
 var server = app.listen(3000, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Now listening at http://%s:%s", host, port)
 })
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var app = express();
+
